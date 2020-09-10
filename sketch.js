4c9b8d636bdf865babdeb85cfe081111ef93eb49
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2020-09-10 17:49:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,16 +7,16 @@ function preload()
  
   //load images here
   img1 = loadImage("images/dogimg.png");
-  img2 = loadImage("images/dogmg1.png");
+  img2 = loadImage("images/dogimg1.png");
 }
 
 function setup() {
   createCanvas(500, 500);
   database = firebase.database();
   foodstock = database.ref('Food');
-  foodstock.on("value",readstock);
+  foodstock.on("value",readstock,showerror);
   dog = createSprite(200,200,20,20);
-  dog.addImage(img1);
+  dog.addImage(img2);
   happyDog = createSprtie(100,100,20,20);
   
   
@@ -29,16 +29,17 @@ function draw() {
   if(keyDown(UP_ARROW)){
     writeFood(-1);
   }else
-  {if(keyDown(DOWN_ARROW)){
+  {
+    if(keyDown(DOWN_ARROW)){
     writeStock(foods);
-    happyDog.addImage(img2);
+    happyDog.addImage(img1);
  }
 }
   drawSprites();
   textSize(20);
   fill('red')
   stroke(4)
-  text("Note: Press UP _ ARROW Key to Feed Dragon Milk")
+  text("Note: Press UP _ ARROW Key to Feed Dragon Milk",200,100);
   //add styles here
 
 }
